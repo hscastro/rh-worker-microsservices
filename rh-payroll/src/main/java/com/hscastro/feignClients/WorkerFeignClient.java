b6343819d37f4e4216f8bde7d92f1ca4203d991f
hscastro <halisonsc5@gmail.com>
2021-02-13 10:51:39 -0300

    add WorkerFeignClient

---
+++ rh-payroll/src/main/java/com/hscastro/feignClients/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package com.hscastro.feignClients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.hscastro.entities.Worker;
+
+@Component
+@FeignClient(name = "rh-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+	
+	@GetMapping(value = "/{id}")	
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
--- rh-payroll/src/main/java/com/hscastro/services/PaymentService.java
+++ rh-payroll/src/main/java/com/hscastro/services/PaymentService.java
@@ -10,23 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.hscastro.entities.Payment;
 import com.hscastro.entities.Worker;
+import com.hscastro.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-
-	@Value("${rh-worker.host}")
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		
-		Map<String, String> uriVariable = new HashMap<>(); 
-		uriVariable.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariable);
-		
+			
+		Worker worker = workerFeignClient.findById(workerId).getBody();		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
