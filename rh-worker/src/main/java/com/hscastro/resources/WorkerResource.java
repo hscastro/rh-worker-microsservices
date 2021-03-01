dfd8624c6d6ddb2cb73a579dfa3e6d74fcce8708
hscastro <halisonsc5@gmail.com>
2021-03-01 18:59:41 -0300

    Important corrections

---
+++ rh-worker/src/main/java/com/hscastro/resources/WorkerResource.java
@@ -0,0 +1,61 @@
+package com.hscastro.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hscastro.entities.Worker;
+import com.hscastro.repositories.WorkerRepository;
+
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(Worker.class);
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private WorkerRepository workerRepository;
+
+	
+	public ResponseEntity<Worker> save(@RequestBody Worker worker) {
+		Worker w = workerRepository.save(worker);
+		return ResponseEntity.ok(w);
+	}
+	
+	@GetMapping(value = "/{id}")	
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+//		try {
+//			Thread.sleep(3000);
+//			
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		logger.info("PORT = "+env.getProperty("local.server.port"));
+		
+		Worker obj = workerRepository.findById(id).get();
+		return ResponseEntity.ok(obj);		
+	}
+	
+	@GetMapping	
+	public ResponseEntity<List<Worker>> findAll(){
+		List<Worker> lista = workerRepository.findAll();
+		return ResponseEntity.ok(lista);		
+	}
+	
+}
