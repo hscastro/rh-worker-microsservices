794a0e71395d9d482a5ff9a7a258da190ecf9183
hscastro <halisonsc5@gmail.com>
2021-03-24 21:44:51 -0300

    Project rh-user created

---
+++ rh-user/src/test/java/com/hscastro/rhuser/RhUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hscastro.rhuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
